080874f20a9b058e5eb8539931ea6e98bfd39868
unknown <abhisurya1421@gmail.com>
2026-07-08 09:59:39 +0530

    DDL & DML Update

--- Lessons/1.16_DDL_DML_1.sql
+++ Lessons/1.16_DDL_DML_1.sql
@@ -1,11 +1,13 @@
 ---  .read Lessons/1.16_DDL_DML_1.sql
 
-
+CREATE DATABASE job_mart;
 -- Show available databases
 SHOW DATABASES;
 
+SELECT * FROM information_schema.schemata;
+
 -- Create the schema if it doesn't exist
-CREATE SCHEMA IF NOT EXISTS staging;
+CREATE SCHEMA IF NOT EXISTS jobs_mart.staging;
 
 -- Create the table in the default schema (main)
 CREATE TABLE IF NOT EXISTS preferred_roles (
@@ -36,3 +38,27 @@ SELECT * FROM staging.preferred_roles;
 
 INSERT INTO staging.preferred_roles(role_id, role_name)
 VALUES(3, 'Software Engineer');
+
+
+
+----------------- DML ----------------------------------------
+
+
+ALTER TABLE staging.preferred_roles
+ADD COLUMN preferred_roles BOOLEAN;
+
+SELECT * FROM staging.preferred_roles;
+
+UPDATE staging.preferred_roles
+SET preferred_roles =TRUE
+WHERE role_id = 1 OR role_id = 2;
+
+UPDATE staging.preferred_roles
+SET preferred_roles = FALSE
+WHERE role_id = 3;
+
+
+SELECT * FROM staging.preferred_roles;
+
+ALTER TABLE staging.preferred_roles
+RENAME TO priority_roles;
