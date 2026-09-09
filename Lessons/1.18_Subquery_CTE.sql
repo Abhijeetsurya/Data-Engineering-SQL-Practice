f1b25f91059a71c466bcd92574cf6badcf8587e1
unknown <abhisurya1421@gmail.com>
2026-09-09 08:00:05 +0530

    CTE

--- Lessons/1.18_Subquery_CTE.sql
+++ Lessons/1.18_Subquery_CTE.sql
@@ -69,3 +69,26 @@ HAVING MEDIAN(salary_year_avg) > (
 )
 LIMIT 10;
 
+
+
+-- CTE Example
+-- Compare how much more (or less) remot roles pay compared to the onsite roles for each job title.
+-- Use a CTE to calculate the median salary by title and work arrangement, then compare those medians.
+    WITH title_medians AS (
+        SELECT 
+            job_title_short,
+            job_work_from_home,
+            MEDIAN(salary_year_avg):: INT AS median_salary
+        FROM job_postings_fact
+        WHERE job_country = 'India'
+        GROUP BY job_title_short, job_work_from_home
+    )
+    SELECT 
+        r.job_title_short,
+        r.median_salary AS remote_median_salary,
+        o.median_salary AS onsite_median_salary
+    FROM title_medians AS r
+    INNER JOIN title_medians AS o
+        r.job_title_short = o.job_title_short
+    WHERE r.job_work_from_home = TRUE;
+
