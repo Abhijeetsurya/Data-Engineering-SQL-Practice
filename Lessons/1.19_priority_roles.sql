61340705a79dff505627fbb5c5545d6127639d3c
unknown <abhisurya1421@gmail.com>
2026-09-10 09:06:56 +0530

    DDL & DML Part 3

---
+++ Lessons/1.19_priority_roles.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE TABLE staging.priority_roles(
+    role_id INTEGER PRIMARY KEY,
+    role_name VARCHAR(255),
+    priority_level INTEGER
+);
+
+
+INSERT INTO staging.priority_roles (role_id, role_name, priority_level)
+VALUES
+    (1, 'Data Engineer', 1),
+    (2, 'Senior Data Engineer', 1),
+    (3, 'Software Engineer', 3);
+
+
+SELECT * FROM staging.priority_roles;
+
