4c44bc3ff875842df3c2be66b32deb31ac65e890
unknown <abhisurya1421@gmail.com>
2026-09-23 09:36:06 +0530

    Date function

--- Lessons/Date_functions.sql
+++ Lessons/Date_functions.sql
@@ -6,3 +6,50 @@ SELECT
     job_posted_date::TIMESTAMPTZ AS timestampz
 FROM job_postings_fact
 LIMIT 10;
+
+
+SELECT 
+    job_posted_date,
+    EXTRACT(YEAR FROM job_posted_date) AS job_posted_year,
+    EXTRACT(MONTH FROM job_posted_date) AS job_posted_month,
+    EXTRACT(DAY FROM job_posted_date) AS job_posted_date
+FROM
+    job_postings_fact
+LIMIT 10;
+
+
+
+
+SELECT 
+    EXTRACT(YEAR FROM job_posted_date) AS job_posted_year,
+    EXTRACT(MONTH FROM job_posted_date) AS job_posted_month,
+    COUNT(job_id) AS job_count
+FROM
+    job_postings_fact
+GROUP BY 
+    EXTRACT(YEAR FROM job_posted_date), 
+    EXTRACT(MONTH FROM job_posted_date) 
+ORDER BY 
+    job_posted_year, job_posted_month;
+
+
+SELECT
+    job_posted_date,
+    DATE_TRUNC('month', job_posted_date) AS job_posted_month
+FROM job_postings_fact
+ORDER BY RANDOM()
+LIMIT 10;
+
+
+SELECT 
+    DATE_TRUNC('month', job_posted_date) AS job_posted_month,
+    COUNT(job_id) AS job_count,
+FROM 
+    job_postings_fact
+WHERE 
+    job_title_short = 'Data Engineer' AND
+    EXTRACT(YEAR FROM job_posted_date) = 2024
+GROUP BY 
+    DATE_TRUNC('month', job_posted_date)
+ORDER BY
+    job_posted_month;
