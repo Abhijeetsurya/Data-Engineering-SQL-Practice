1f3181c16007c21706dabb15ba8cb85b68007384
unknown <abhisurya1421@gmail.com>
2026-09-26 14:44:32 +0530

    .

--- Lessons/CASE_Expression_Practice.sql
+++ Lessons/CASE_Expression_Practice.sql
@@ -105,7 +105,7 @@ SELECT
     CASE
         WHEN salary_year_avg >= 200000 THEN 'Very High'
         WHEN salary_year_avg >= 150000 THEN 'High'
-        WHEN salary_year_avg >= 100000 THEN 'Meduim'
+        WHEN salary_year_avg >= 100000 THEN 'Medium'
         WHEN salary_year_avg < 100000 THEN 'Low'
     END AS Data_enginer_salary
 FROM
@@ -140,3 +140,84 @@ SELECT
 FROM
     job_postings_fact
 WHERE salary_year_avg IS NOT NULL;
+
+
+
+
+/* Q147
+Classify job_schedule_type:
+- Full Time
+- Part Time
+- Contract
+- Other  */
+
+SELECT 
+    job_schedule_type,
+    CASE
+        WHEN job_schedule_type = 'Full-Time' THEN 'Full Time'
+        WHEN job_schedule_type = 'Part-Time' THEN 'Part Time'
+        WHEN job_schedule_type = 'Contract' THEN 'contact'
+        ELSE 'Other'
+    END AS job_schedule_category
+FROM job_postings_fact;
+
+
+/* Q148
+Create salary ranges:
+- 0-50K
+- 50K-100K
+- 100K-150K
+- 150K+ */
+
+SELECT
+    salary_year_avg,
+    CASE 
+        WHEN salary_year_avg BETWEEN 0 AND 50000 THEN '0-50K'
+        WHEN salary_year_avg BETWEEN 50000 AND 100000 THEN '50K-100K'
+        WHEN salary_year_avg BETWEEN 100000 AND 150000 THEN '100K-150K'
+        ELSE '150K+'
+    END AS Salary_range
+FROM
+    job_postings_fact
+WHERE salary_year_avg IS NOT NULL;
+
+
+/* Q149
+Create a salary data-quality status:
+- Valid → salary > 0
+- Invalid → salary <= 0
+- Missing → salary IS NULL */
+
+SELECT 
+    salary_year_avg, salary_hour_avg,
+    CASE
+        WHEN salary_year_avg > 0 OR salary_hour_avg > 0 THEN 'Valid'
+        WHEN salary_year_avg <= 0 AND salary_hour_avg <= 0 THEN 'Invalid'
+        WHEN salary_year_avg IS NULL OR salary_hour_avg IS NULL THEN 'Missing'
+    END AS salary_data_quality_status
+FROM
+    job_postings_fact
+WHERE salary_year_avg IS NOT NULL OR salary_hour_avg IS NOT NULL;
+
+
+
+/* Q150
+Classify:
+- Data Engineer → Engineering
+- Data Scientist → Data Science
+- Data Analyst → Analytics
+- Machine Learning Engineer → ML Engineering
+- Everything else → Other */
+
+SELECT 
+    job_title,
+    CASE
+        WHEN job_title LIKE '%Data%' AND job_title LIKE '%Engineer%' THEN 'Engineering'
+        WHEN job_title LIKE '%Data%' AND job_title LIKE '%Scientist%' THEN 'Data Science'
+        WHEN job_title LIKE '%Data%' AND job_title LIKE '%Analyst%' THEN 'Analytics'
+        WHEN job_title LIKE '%Machine%' AND job_title LIKE  '%Learning%' OR job_title LIKE '%Engineer%' THEN 'ML Engineering'
+        ELSE 'Other'
+    END AS job_classification
+FROM
+    job_postings_fact
+GROUP BY job_title;
